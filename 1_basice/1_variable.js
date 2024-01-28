8a9f2d0c7e5c2b063fb35f8808f977b9cb0074ea
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-01-28 14:54:27 +0000

    variable-and-datatype

---
+++ 1_basice/1_variable.js
@@ -0,0 +1,12 @@
+const accountId = 14453;
+let accountEmail = "priyal@gmailcom";
+var accountPassword = "123456";
+accountCity = "surat" ;
+let accountState 
+
+
+// accountId = 123;
+accountEmail = "goti@gamil.com";
+accountPassword = "656565";
+accountCity = "ahemdabad";
+console.table([accountEmail,accountPassword,accountCity,accountStatem])
---
+++ 1_basice/2_datatype.js
@@ -0,0 +1,12 @@
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol => unique
+
+
+//object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
