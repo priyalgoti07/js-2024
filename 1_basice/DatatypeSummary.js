61c3d8ee306e5ac5c1e09ccea90b13a145ff82f5
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-01-30 17:37:22 +0000

    Datatype-summary

---
+++ 1_basice/DatatypeSummary.js
@@ -0,0 +1,10 @@
+//Premitive
+
+//type 7 : String,Number,boolean,null,undefiend,BigInt
+
+// Reference (Non Premitive)
+
+// Array, object,function
+
+const bigNumber = 122313132131331n
+console.log("bigNumber",typeof(bigNumber));
