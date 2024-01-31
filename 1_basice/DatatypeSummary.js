d36594b2ac462cb080b8b36dc3358598a97496d5
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-01-31 08:12:47 +0000

    memory-concepts

--- 1_basice/DatatypeSummary.js
+++ 1_basice/DatatypeSummary.js
@@ -7,4 +7,24 @@
 // Array, object,function
 
 const bigNumber = 122313132131331n
-console.log("bigNumber",typeof(bigNumber));
+console.log("bigNumber", typeof (bigNumber));
+
+//Stack(Primtive) and Heap(Non-Primitive)
+
+//Stack : copy to  variable 
+
+//Heap: original value reference
+
+let myname = "Priyal"
+let copyName = myname
+console.log("myname",myname);
+console.log("copyName",copyName);
+
+let userOne ={
+    emil: "priyalgoti@gmial.com",
+    upi: "123123"
+}
+let userTwo = userOne
+userTwo.emil= "GheavriyaPriyal@gmail.com"
+console.log("userOne",userOne);
+console.log("userTwo",userTwo);
