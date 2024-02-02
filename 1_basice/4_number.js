aa0a0d44b370af2efa1b16a68882fbe6c5703236
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-02-02 16:30:46 +0000

    number

---
+++ 1_basice/4_number.js
@@ -0,0 +1,14 @@
+let score = 400
+console.log("score",score); 
+let balance = new Number(100)
+console.log("balance",balance.toString());
+
+const hundred = 1000000
+console.log("hundred",hundred.toLocaleString('en-IN'));
+console.log(Math.abs(-4));
+console.log(Math.round(4.9));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.5));
+console.log(Math.min(4,6,8));
+console.log(Math.max(8,6,3));
