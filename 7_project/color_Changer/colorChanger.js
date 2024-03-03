5659304d881c9c91c1f5e63584e55b16675ad072
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-03-03 12:28:21 +0000

    use swith case

--- 7_project/color_Changer/colorChanger.js
+++ 7_project/color_Changer/colorChanger.js
@@ -6,20 +6,32 @@ button.forEach((btn) => {
     btn.addEventListener("click", function (e) {
         // console.log(e);
         console.log(e.target.id);
-        if (e.target.id === "grey") {
-            body.style.backgroundColor = e.target.id
-        }
-        else if (e.target.id === "white") {
-            body.style.backgroundColor = e.target.id
-        }
-        else if (e.target.id === "blue") {
-            body.style.backgroundColor = e.target.id
-        }
-        else if (e.target.id === "yellow") {
-            body.style.backgroundColor = e.target.id
-        }
-        else if (e.target.id === "purpel") {
-            body.style.backgroundColor = e.target.id
+        // if (e.target.id === "grey") {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // else if (e.target.id === "white") {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // else if (e.target.id === "blue") {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // else if (e.target.id === "yellow") {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        // else if (e.target.id === "purpel") {
+        //     body.style.backgroundColor = e.target.id
+        // }
+        switch (e.target.id) {
+            case "grey": body.style.backgroundColor = e.target.id
+                break;
+            case "white": body.style.backgroundColor = e.target.id
+                break;
+            case "blue": body.style.backgroundColor = e.target.id
+                break;
+            case "yellow": body.style.backgroundColor = e.target.id
+                break;
+            default: body.style.backgroundColor = "#9C27B0"
+                break;
         }
 
     })
