30180686571cf4b4def07736472bc57c39823e0e
priyal <107545908+priyalgoti07@users.noreply.github.com>
2024-03-04 10:51:08 +0000

    3project_gesstheNumber

---
+++ 7_project/GuessTheNumber/guessThenumber.js
@@ -0,0 +1,55 @@
+// const rdn = parseInt(Math.random() * 100 + 1)
+console.log('rdn');
+
+// const submit = document.querySelector('#subt');
+// console.log("submit",submit);
+// const userInput = document.querySelector('#guessField');
+// const guessslot = document.querySelector('.guesses');
+// const remining = document.querySelector('.lastResult');
+// const lowOrHi = document.querySelector('.lowOrHi');
+// const startover = document.querySelector('.resultParas');
+
+// //craete Element
+// const p = document.createElement('p');
+
+// //prevGuess submited all value store in Prevguess
+// const prevGuess = [];
+
+// //how many attempt user
+// const numGuess = 1;
+
+// //game started
+// let playGame = true;
+
+// console.log("pal",playGame);
+// if(playGame){
+//     submit.addEventListener('click', function(e){
+//         e.preventDefault()
+//     })
+// }
+
+// //ValidateGuess number is valid or not, not string , value is not 1 less then and value is not a nagitive
+// function validateGuess(guess) {
+
+// }
+// //guess Messgage print
+// function checkGuess(guess) {
+
+// }
+
+// function displayGuess(guess){
+
+// }
+
+// //interrupted to direct Dom  & DOM mainupulation
+// function displayMessge(guess){
+
+// }
+
+// function overGame(){
+
+// }
+
+// function newgame(){
+
+// }
