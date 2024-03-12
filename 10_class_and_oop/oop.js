1d5a946b6feb97e199e0a8bf1c07d4fb2366d4d5
priyalgoti07 <gotipriyal1996@gmail.com>
2024-03-12 21:51:21 +0530

    counstructore

--- 10_class_and_oop/oop.js
+++ 10_class_and_oop/oop.js
@@ -1,6 +1,25 @@
+const gloable = "Mahadev"
 const user = {
     username: "priyal",
     logincount: 8,
-    signedIn: true
+    signedIn: true,
+    getUserDeails: function () {
+        // console.log("Got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
 }
 console.log(user.signedIn);
+// console.log(user.getUserDeails());
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    return this
+}
+
+const userdata = User("priyal",2,true)
+console.log(userdata);
