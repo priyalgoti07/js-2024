f8b0a8ddc8d2202c1a01d415f1880dbe2341eb5c
priyal.finestar <priyal@finestar.in>
2024-03-23 10:59:37 +0530

    ProtoypeCompleted

--- 10_class_and_oop/Prototype.js
+++ 10_class_and_oop/Prototype.js
@@ -1,5 +1,5 @@
 let myGod = "Mahadev"
-console.log(myGod.length);
+// console.log(myGod.length);
 
 let Myheros = ["Mahadev", "Ram"]
 
@@ -7,17 +7,26 @@ let heroPower = {
     Mahadev: "parvtima",
     Ram: "Sitama",
      getGodpower : function(){
-        console.log(`god power is ${this.Ram}`);
+        // console.log(`god power is ${this.Ram}`);
      }
 }
 
 Object.prototype.God = function(){
-    console.log(`my god is Present in all objects`);
+    // console.log(`my god is Present in all objects`);
 }
 
 Array.prototype.heygod =function(){
-    console.log(`Bhagvan is always Right`);
+    // console.log(`Bhagvan is always Right`);
 }
 Myheros.God()
 Myheros.heygod()
 // heroPower.heygod()
+
+let anotherUsername = "Priyal with JavaSacript"
+
+String.prototype.trLength = function(){
+    console.log( `${this}`)
+    console.log(`True length is : ${this.length}`)
+}
+anotherUsername.trLength()
+"om".trLength()
